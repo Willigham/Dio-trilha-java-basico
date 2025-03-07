6610cfe88453471c18669ab000605d6c3541420f
Willigham Coelho <willigamcoelho@gmail.com>
2025-03-07 11:49:41 -0300

    update processoSeletivo

--- controle-candidatos/src/ProcessoSeletivo2.java
+++ controle-candidatos/src/ProcessoSeletivo2.java
@@ -3,14 +3,26 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo2 {
     public static void main(String[] args) {
-        selecaoCandidatos();
+       
     }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELITE","JULIA","PAULO","AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidate de nº " + (indice+1) + " é " + candidatos[indice]);
+        }
+    }
+
     static void selecaoCandidatos(){
         String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MILENA","DANIELA","JORGE"};
-
+    
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
         double salarioBase = 2000.0;
+
         while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
             String candidato = candidatos[candidatosAtual];
             double salarioPretendido = valorPretendido();
@@ -29,10 +41,13 @@ public class ProcessoSeletivo2 {
 
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
+
         if(salarioBase > salarioPretendido){
             System.out.println("LIGAR PARA O CANDIDATO");
+
         }else if (salarioBase==salarioPretendido)
-            System.out.println("LIGAR PARA O CANDIDTO COM CONTRA PROPOSTA");   
+            System.out.println("LIGAR PARA O CANDIDTO COM CONTRA PROPOSTA"); 
+              
         else {
             System.out.println("AGUARDAR O RESULTADO DOS DEMAIS CANDIDATOS");
         } 
