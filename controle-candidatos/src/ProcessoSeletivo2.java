425996f85e50a9fb3df3d287adc7812ace606586
Willigham Coelho <willigamcoelho@gmail.com>
2025-03-07 11:40:46 -0300

    atualizei

---
+++ controle-candidatos/src/ProcessoSeletivo2.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class ProcessoSeletivo2 {
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MILENA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if (salarioBase==salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDTO COM CONTRA PROPOSTA");   
+        else {
+            System.out.println("AGUARDAR O RESULTADO DOS DEMAIS CANDIDATOS");
+        } 
+    }
+}
